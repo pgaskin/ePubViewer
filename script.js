5245d68212a69d71e4db6e16307a09f687585f6c
Patrick G <geek1011@outlook.com>
2017-08-12 13:55:34 -0400

    Fix swipe detection

--- script.js
+++ script.js
@@ -643,7 +643,7 @@ ePubViewer.init = function () {
 
     EPUBJS.Hooks.register('beforeChapterDisplay').swipeDetection = function (callback, renderer) {
         var swiper = renderer.doc.createElement('script');
-        swiper.innerHTML = 'function Swiper(b,e,f,g,h){var c=null,d=null;b.addEventListener("touchstart",function(a){c=a.touches[0].clientX;d=a.touches[0].clientY},!1);b.addEventListener("touchmove",function(a){if(c&&d){var b=c-a.touches[0].clientX;a=d-a.touches[0].clientY;Math.abs(b)>Math.abs(a)?0<b?e():f():0<a?g():h();c=d=null}},!1)};Swiper(document,function(){parent.ePubViewer.actions.nextPage()},function(){parent.ePubViewer.actions.nextPage()},function(){},function(){});';
+        swiper.innerHTML = 'function Swiper(b,e,f,g,h){var c=null,d=null;b.addEventListener("touchstart",function(a){c=a.touches[0].clientX;d=a.touches[0].clientY},!1);b.addEventListener("touchmove",function(a){if(c&&d){var b=c-a.touches[0].clientX;a=d-a.touches[0].clientY;Math.abs(b)>Math.abs(a)?0<b?e():f():0<a?g():h();c=d=null}},!1)};Swiper(document,function(){parent.ePubViewer.actions.nextPage()},function(){parent.ePubViewer.actions.prevPage()},function(){},function(){});';
         renderer.doc.head.appendChild(swiper);
         if (callback) callback();
     };
